dc6d5cda95602582c5ee70f44a8435dd94fb3a1a
Vasiliy <xded@mail.ru>
2021-01-16 21:57:32 +0000

    exam is pass

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
-sudo rm -rf /etc/nginx/sites-enabled/default.conf
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/conf.d/default.conf
+sudo rm -rf /etc/nginx/conf.d/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/conf.d/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart

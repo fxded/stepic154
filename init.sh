bc21ee6ee015cffff859f6d552c6e6f68df09118
Vasiliy Votintseff <fxded@mail.ru>
2021-01-17 00:42:12 +0300

    ready to exam3

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo rm -rf /etc/nginx/conf.d/default.conf
+sudo rm -rf /etc/nginx/sites-enabled/default.conf
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/conf.d/default.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test

dbae972f09dcb79a0cad45295fa42dc713e30fb1
Vasiliy <fxded@mail.ru>
2021-02-22 00:13:13 +0300

    pass exam1.9

--- init.sh
+++ init.sh
@@ -3,6 +3,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/conf.d/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:resp_app
 sudo /etc/init.d/mysql start

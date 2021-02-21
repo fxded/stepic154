dd92213cd583e121684170a36a2fcb69d94068a9
Vasiliy <fxded@mail.ru>
2021-02-21 23:49:17 +0300

    ready to exam1.9

--- python/hello.py
+++ python/hello.py
@@ -1,5 +1,4 @@
 def resp_app(env, start_response):
-    data = b'Hello from python3!'
-    print (env)
+    data = "\n".join(env.get('QUERY_STRING').split("&"))
     start_response('200 OK', [('Content-Type', 'text/plain'), ('Content-Length', str(len(data)))])
-    return iter([data])
+    return [bytes(data.encode())]
